196088b838881a306f9bff3243cd9bdeb09d655a
kata-v <7273558+kata-v@users.noreply.github.com>
2020-09-23 11:18:20 +0300

    Added rest of the code from the tutorial but there is still a bug somewhere

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { StyledApp } from './AppStyles';
+import { createCalendar } from './helpers';
+import Hatch from './Hatch';
 
+const GlobalStyle = createGlobalStyle`
+body {
+  background: center / cover url('./img/calendar_backdrop.jpg');
+  margin: 0;
+}
+`
 
 function App() {
+const [hatches, setHatches] = useState([]);
+
+useEffect(() => {
+  hatches.length && localStorage.setItem('calendar', JSON.stringify(hatches));
+}, [hatches]);
+
+useEffect(() => {
+  const calendar = localStorage.calendar
+    ? JSON.parse(localStorage.calendar)
+    : createCalendar();
+
+  setHatches(calendar);
+}, []);
+
+// useEffect(()=> {
+//   hatches.length && localStorage.setItem("calendar", JSON.stringify(hatches));
+// }, [hatches]);
+
+// useEffect(()=> {
+//   const calendar = localStorage.calendar
+//   ? JSON.parse(localStorage.calendar)
+//   : createCalendar();
+
+//   setHatches(calendar);
+// }, []);
+
+
+
+const handleFlipHatch= id => {
+  const updatedHatches = hatches.map(hatch =>
+    hatch.id === id ? {...hatch, open: !hatch.open}: hatch
+    );
+    setHatches(updatedHatches);
+};
+
   return (
     <>
-      Advent Calendar
+    <GlobalStyle/>
+    <StyledApp>
+      {hatches.map(hatch=> (
+      <Hatch 
+      key={hatch.id}
+      hatchData={hatch}
+      handleClick={handleFlipHatch}
+      />))}
+      </StyledApp>
     </>
   );
 }
